11581db8bd431792cde8b4f148c7a10284aa81a0
UBamtol <yij0325@gmail.com>
2024-09-10 02:18:04 +0900

    [ Web ] Feat main page, chatbox

--- doctor_p_web/src/app/page.tsx
+++ doctor_p_web/src/app/page.tsx
@@ -1,101 +1,85 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import drPetLogo from './public/images/DrPET_Logo.svg';
+import dogBotton from './public/images/Dr.PET_dogBotton.svg';
+import catBotton from './public/images/Dr.PET_catBotton.svg';
+import chatBotton from './public/images/Dr.PET_chat.svg';
+import { useState } from 'react';
+import {
+  XCircleIcon,
+  ChatBubbleLeftRightIcon,
+} from '@heroicons/react/24/outline';
 
 export default function Home() {
+  const [isChatClicked, setIsChatClicked] = useState(false);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className='flex justify-center border border-red-600 w-full h-screen'>
+      <div className='relative flex justify-center h-fit'>
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          // className='ml-[20px] mr-[5px]'
+          className='h-fit'
+          src={drPetLogo}
+          width={600}
+          height={600}
+          alt='Doctor.PET logo'
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div className='flex absolute -bottom-[10px] space-x-[50px]'>
+          <div className='flex justify-center items-center w-[130px] h-[130px] rounded-full bg-[#F7D7D7] shadow-br-sm hover:cursor-pointer'>
             <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className='h-fit'
+              src={dogBotton}
+              alt='dogBotton'
+              width={90}
+              height={90}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className='flex justify-center items-center w-[130px] h-[130px] rounded-full bg-[#C9EAFD] shadow-br-sm hover:cursor-pointer'>
+            <Image
+              className='h-fit'
+              src={catBotton}
+              alt='dogBotton'
+              width={90}
+              height={90}
+            />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div
+        className={`flex justify-center items-center absolute bottom-14 right-14 w-[60px] h-[60px] bg-[#DAEEF2] rounded-full hover:cursor-pointer ${
+          isChatClicked ? '' : 'shadow-br-sm '
+        }`}
+        onClick={() => setIsChatClicked(!isChatClicked)}
+      >
+        <Image
+          className='h-fit'
+          src={chatBotton}
+          alt='chatBotton'
+          width={40}
+          height={40}
+        />
+      </div>
+      {isChatClicked && (
+        <div className='absolute right-14 bottom-14 w-[400px] h-[650px] rounded-3xl bg-white shadow-br-sm'>
+          <div className='flex items-center justify-between px-5 w-full h-[50px] rounded-t-3xl bg-[#FFD7A3]'>
+            <div className='flex space-x-2 items-center'>
+              <div className='w-5 h-5 rounded-full bg-[#93EF97]'></div>
+              <div>Chat Dr.</div>
+            </div>
+            <XCircleIcon
+              className='w-6 h-6 hover:cursor-pointer text-[#867373]'
+              onClick={() => setIsChatClicked(!isChatClicked)}
+            ></XCircleIcon>
+          </div>
+          <div className='w-full h-[550px] border border-red-500'>챗박스</div>
+          <div className='flex items-center px-5 space-x-2 w-full h-[50px] rounded-b-3xl bg-[#FFD7A3]'>
+            <input
+              className='bg-[#FFD7A3] w-full h-fit'
+              placeholder='Send a message...'
+            ></input>
+            <ChatBubbleLeftRightIcon className='w-6 h-6 hover:cursor-pointer text-[#867373]'></ChatBubbleLeftRightIcon>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- doctor_p_web/tailwind.config.ts
+++ doctor_p_web/tailwind.config.ts
@@ -1,16 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      boxShadow: {
+        'top-xl': '0 -5px 40px rgba(0, 0, 0, 0.2)',
+        'top-md': '0 -2px 5px rgba(0, 0, 0, 0.2)',
+        'bottom-md': '0 0px 5px rgba(0, 0, 0, 0.2)',
+        'bottom-sm': '0 4px 5px -5px rgba(0, 0, 0, 0.2)',
+        'y-md': '0 0px 5px rgba(0, 0, 0, 0.2)',
+        'br-sm': '4px 4px 6px 0px rgba(0, 0, 0, 0.2)',
       },
     },
   },
