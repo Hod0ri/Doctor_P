099a8113f77f73124b2b59771a3030cb8867eedd
UBamtol <yij0325@gmail.com>
2024-09-10 21:38:50 +0900

    [ Web ] Fix main page

--- doctor_p_web/src/app/page.tsx
+++ doctor_p_web/src/app/page.tsx
@@ -9,22 +9,28 @@ import {
   XCircleIcon,
   ChatBubbleLeftRightIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 export default function Home() {
   const [isChatClicked, setIsChatClicked] = useState(false);
   return (
-    <div className='flex justify-center border border-red-600 w-full h-screen'>
-      <div className='relative flex justify-center h-fit'>
+    <div className='flex justify-center w-full h-screen'>
+      <div className='relative flex justify-center w-full h-full border border-red-200'>
         <Image
           // className='ml-[20px] mr-[5px]'
-          className='h-fit'
+          className='w-fit h-fit'
           src={drPetLogo}
           width={600}
           height={600}
+          // fill
           alt='Doctor.PET logo'
+          // sizes='(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw'
         />
-        <div className='flex absolute -bottom-[10px] space-x-[50px]'>
-          <div className='flex justify-center items-center w-[130px] h-[130px] rounded-full bg-[#F7D7D7] shadow-br-sm hover:cursor-pointer'>
+        <div className='flex absolute top-[400px] space-x-[50px]'>
+          <Link
+            className='flex justify-center items-center w-[130px] h-[130px] rounded-full bg-[#F7D7D7] shadow-br-sm hover:cursor-pointer'
+            href='/pages/searchDog'
+          >
             <Image
               className='h-fit'
               src={dogBotton}
@@ -32,8 +38,11 @@ export default function Home() {
               width={90}
               height={90}
             />
-          </div>
-          <div className='flex justify-center items-center w-[130px] h-[130px] rounded-full bg-[#C9EAFD] shadow-br-sm hover:cursor-pointer'>
+          </Link>
+          <Link
+            className='flex justify-center items-center w-[130px] h-[130px] rounded-full bg-[#C9EAFD] shadow-br-sm hover:cursor-pointer'
+            href='/pages/searchCat'
+          >
             <Image
               className='h-fit'
               src={catBotton}
@@ -41,7 +50,7 @@ export default function Home() {
               width={90}
               height={90}
             />
-          </div>
+          </Link>
         </div>
       </div>
       <div
@@ -59,7 +68,7 @@ export default function Home() {
         />
       </div>
       {isChatClicked && (
-        <div className='absolute right-14 bottom-14 w-[400px] h-[650px] rounded-3xl bg-white shadow-br-sm'>
+        <div className='absolute right-14 bottom-14 w-[350px] h-5/6 rounded-3xl bg-white shadow-br-sm pb-[50px]'>
           <div className='flex items-center justify-between px-5 w-full h-[50px] rounded-t-3xl bg-[#FFD7A3]'>
             <div className='flex space-x-2 items-center'>
               <div className='w-5 h-5 rounded-full bg-[#93EF97]'></div>
@@ -70,8 +79,8 @@ export default function Home() {
               onClick={() => setIsChatClicked(!isChatClicked)}
             ></XCircleIcon>
           </div>
-          <div className='w-full h-[550px] border border-red-500'>챗박스</div>
-          <div className='flex items-center px-5 space-x-2 w-full h-[50px] rounded-b-3xl bg-[#FFD7A3]'>
+          <div className='w-full h-full'>챗박스</div>
+          <div className='flex items-center px-5 space-x-2 w-full h-[50px] relative bottom-[50px] rounded-b-3xl bg-[#FFD7A3] shadow-br-sm'>
             <input
               className='bg-[#FFD7A3] w-full h-fit'
               placeholder='Send a message...'
