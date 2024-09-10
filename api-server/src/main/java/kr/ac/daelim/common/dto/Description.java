2e7455cf5a3f80205208a2b2c6c4eff2a8b9c6a6
맹성호 <seongho.maeng@apples.re.kr>
2024-09-10 17:28:28 +0900

    Description 파일명 수

--- api-server/src/main/java/kr/ac/daelim/common/dto/Description.java
+++ api-server/src/main/java/kr/ac/daelim/common/dto/Description.java
@@ -1,20 +1,16 @@
 package kr.ac.daelim.search.dto;
 
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @Builder
 @Data
 @JsonNaming(PropertyNamingStrategies.UpperSnakeCaseStrategy.class)
-public class SearchResDTO {
+public class Description {
     @JsonProperty(value="ko-KR")
     private String ko;
     private String symptom;
