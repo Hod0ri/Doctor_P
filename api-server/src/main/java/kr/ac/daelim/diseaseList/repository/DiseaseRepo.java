ca8369a4ff3b148b05920186ace363426b2b7c77
맹성호 <seongho.maeng@apples.re.kr>
2024-09-24 10:37:09 +0900

    [LINK-001] 질병 리스트 조회 쿼리 메소드 수

--- api-server/src/main/java/kr/ac/daelim/diseaseList/repository/DiseaseRepo.java
+++ api-server/src/main/java/kr/ac/daelim/diseaseList/repository/DiseaseRepo.java
@@ -3,15 +3,16 @@ package kr.ac.daelim.diseaseList.repository;
 import java.util.List;
 import java.util.Optional;
 
-import kr.ac.daelim.diseaseList.entity.Disease;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import kr.ac.daelim.diseaseList.entity.Disease;
 
 @Repository
 public interface DiseaseRepo extends JpaRepository<Disease, String > {
 	List<Disease> findAllByFamily(String family);
 
-	List<Disease> findByIdAndFamily(Iterable<String> ids, String family);
+	List<Disease> findByFamilyAndIdIn(String family, Iterable<String> ids);
 
 	Optional<Disease> findByIdAndFamily(String id, String family);
 }
